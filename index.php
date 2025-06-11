b396f38daf2cab5157abbc85d09c2ff3daf2ffe3
AlessonBarrosRepositorio <84032869+AlessonBarrosRepositorio@users.noreply.github.com>
2025-06-11 10:47:42 -0300

    ...

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gráfica Rápida</title>
+</head>
+<body>
+    <div class="top">
+        
+    </div>
+    <div class="midle"></div>
+    <div class="bottom"></div>
+</body>
+</html>
