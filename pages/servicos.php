30fded023eab4f2a5cbcc61d8eb4fda853a70e7a
AlessonBarrosRepositorio <84032869+AlessonBarrosRepositorio@users.noreply.github.com>
2025-06-16 11:44:05 -0300

    ....

--- pages/servicos.php
+++ pages/servicos.php
@@ -3,9 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gráfica Rápida</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../midle.css">
+    <link id="cardCSS" rel="stylesheet" href="../card.css">
+    
+    <link id="cardCSS" rel="stylesheet" href="../reset.css">
+
+    <script src="../cardScript/cardsCadastrados.js" defer></script>
+    <script src="../cardScript/btnCardNav.js" defer></script>
+    <script src="../cardScript/multiplicarSlider.js" defer></script>
 </head>
 <body>
-    <a href="../index.php"><-</a>
+        <a style="z-index:9999;" href="../index.php"><-</a>
+    <div class="top">
+        <div class="topArea">
+            <div class="btn-whatsapp">
+                <div class="areaBtn-whatsapp">
+                    <a href="https://wa.me/+5521978841430">
+                        <div class="areaImagemA">
+                            <img src="./wb.png" alt="imagem whatsapp" srcset="">
+                        </div>                        
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="midle">
+
+        <div class="editavel">
+            <div class="areaEditavel">
+                <div class="conteudo">
+                    <div class="areaConteudo">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom"></div>
 </body>
 </html>
